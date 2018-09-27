37347df1cff2531c5ec033603a39725b73478a54
Albert Kaaman <albert@kaaman.nu>
2018-09-27 18:56:20 +0200

    chore: initial commit

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,59 @@
+<script>
+import VInput from 'vuetify/es5/components/VInput'
+import VBtn from 'vuetify/es5/components/VBtn'
+import VIcon from 'vuetify/es5/components/VIcon'
+import VChip from 'vuetify/es5/components/VChip'
+
+export default {
+  extends: VInput,
+
+  methods: {
+    genDefaultSlot () {
+      return [
+        this.genLabel(),
+        this.genCustomInput(),
+        ...this.genButtons()
+      ]
+    },
+    genCustomInput () {
+      return this.$createElement(VChip, {
+        props: {
+          color: this.validationState
+        }
+      }, [this.internalValue])
+    },
+    genButtons () {
+      const v = this.internalValue.split()
+      return [
+        this.$createElement(VBtn, {
+          props: {
+            small: true,
+            icon: true
+          },
+          on: {
+            click: () => {
+              this.$emit('input', (Number(this.internalValue) + 1).toString())
+            }
+          }
+        }, [this.$createElement(VIcon, ['add'])]),
+        this.$createElement(VBtn, {
+          props: {
+            small: true,
+            icon: true
+          },
+          on: {
+            click: () => {
+              this.$emit('input', (Number(this.internalValue) - 1).toString())
+            }
+          }
+        }, [this.$createElement(VIcon, ['remove'])])
+      ]
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
